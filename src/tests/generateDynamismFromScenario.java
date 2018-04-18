194e40188334a84baea13e2248439c04e70af400
1715818 <1715818@RG-N436-07.rgu.ac.uk>
2018-04-18 17:13:28 +0100

    Some dynamic environment created and made the plots of the results of size 200.

--- src/tests/generateDynamismFromScenario.java
+++ src/tests/generateDynamismFromScenario.java
@@ -7,14 +7,18 @@ public class generateDynamismFromScenario {
 
 	public static void main(String[] args) throws IOException {
         
-        String[] bigScenario = {"./data/dynamic/dynProfile-n500-c1000-Cayley5.txt"};
-        int[] nChanges = {3};
+		int[] sizes = {100};
+        int[] bigScenario = {60};
+        int[] nChanges = {10};
         
-        for(String i:bigScenario){
+        for(int i:bigScenario){
         	for(int j:nChanges){
-        		String[] str = new String[] {i, Integer.toString(j)};
-        		
-        		DynamicBenchmarkFromScenario.main(str);
+        		for (int size:sizes){
+	        		String dynPath = "./data/dynamic/dynProfile-n"+size+"-c1000-Cayley" + i + ".txt";
+	        		String[] str = new String[] {dynPath, Integer.toString(j)};
+	        		
+	        		DynamicBenchmarkFromScenario.main(str);
+        		}
         	}
         }
 	}
